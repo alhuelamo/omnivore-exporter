9c0ae8a0c025c7786d0ec2f6303e3f73d7aafd47
Alberto Huélamo <alhuelamo+code@gmail.com>
2024-06-24 20:11:01 +0200

    CLI args

--- src/main/com/alhuelamo/omnivore/exporter/config/AppConf.scala
+++ src/main/com/alhuelamo/omnivore/exporter/config/AppConf.scala
@@ -1,6 +1,8 @@
 package com.alhuelamo.omnivore.exporter.config
 
-import com.alhuelamo.omnivore.exporter.config.cli.entrypoint
+import cats.implicits.*
+import com.alhuelamo.omnivore.exporter.config.cli.cli
+import com.monovore.decline.Command
 
 case class AppConf(
     apiToken: String,
@@ -8,6 +10,13 @@ case class AppConf(
 )
 
 object AppConf {
+
+  private val configFromCli = (
+    cli.apiToken,
+    cli.inputLabel,
+  ).mapN(AppConf.apply)
+
+  private val entrypoint: Command[AppConf] = Command("omnivore-exporter", "Export Omnivore data")(configFromCli)
 
   def apply(args: Array[String]): AppConf =
     entrypoint.parse(args) match {
--- src/main/com/alhuelamo/omnivore/exporter/config/cli.scala
+++ src/main/com/alhuelamo/omnivore/exporter/config/cli.scala
@@ -1,17 +1,8 @@
 package com.alhuelamo.omnivore.exporter.config.cli
 
-import cats.implicits.*
-import com.alhuelamo.omnivore.exporter.config.AppConf
-import com.monovore.decline.{Command, Opts}
+import com.monovore.decline.Opts
 
 object cli {
   val apiToken: Opts[String] = Opts.option[String]("api-token", help = "The Omnivore API token")
   val inputLabel: Opts[Option[String]] = Opts.option[String]("input-label", help = "The input label").orNone
 }
-
-val configFromCli = (
-  cli.apiToken,
-  cli.inputLabel,
-).mapN(AppConf.apply)
-
-val entrypoint: Command[AppConf] = Command("omnivore-exporter", "Export Omnivore data")(configFromCli)
